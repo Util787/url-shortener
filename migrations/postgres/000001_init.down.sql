368a2446d2698ba777abf9537d369aa5370381c7
Util787 <georgiy.chuprina.787@mail.ru>
2026-02-21 11:33:17 +0300

    added migrations

---
+++ migrations/postgres/000001_init.down.sql
@@ -0,0 +1,5 @@
+DROP INDEX IF EXISTS idx_long_url;
+DROP INDEX IF EXISTS idx_short_url;
+DROP INDEX IF EXISTS idx_created_at;
+
+DROP TABLE IF EXISTS url_mappings;
---
+++ migrations/postgres/000001_init.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS url_mappings (
+    id VARCHAR(255) PRIMARY KEY,
+    long_url TEXT NOT NULL,
+    short_url VARCHAR(255) NOT NULL UNIQUE,
+    created_at BIGINT NOT NULL,
+    updated_at BIGINT
+);
+
+CREATE INDEX idx_long_url ON url_mappings(long_url);
+CREATE INDEX idx_short_url ON url_mappings(short_url);
+CREATE INDEX idx_created_at ON url_mappings(created_at);
